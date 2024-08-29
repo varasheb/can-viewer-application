4fe7d109789c4f1b9be392a51b6221334405d442
Anandswamy18 <anandsmathpati18@gmail.com>
2024-08-29 15:31:09 +0530

    [Anand] integrted sitch case with functions

--- src/decodeframe.js
+++ src/decodeframe.js
@@ -20,11 +20,27 @@ export function decodeFrame(data) {
         break;
 
       case '01':
-        break;
+        return {
+          id: '01',
+          name: 'Monitor status since DTCs Cleared',
+          value: decodedata.monitor_status_since_DTCs_cleared(hexdata)
+        };
       case '02':
-        break;
+        return {
+          id: '02',
+          name: 'Freez dtc frame stored',
+          value: decodedata.freez_dtc(hexdata)
+
+        }
       case '03':
-        break;
+        return {
+
+          id: '03',
+          name: 'Fuel system status',
+          value: decodedata.fuel_system_status(hexdata)
+
+        }
+
       case '04':
         return {
           id: '04',
@@ -44,11 +60,23 @@ export function decodeFrame(data) {
           value: decodedata.calculate_fuel_Trim(hexdata)
         };
       case '07':
-        break;
+        return {
+          id: '07',
+          name: 'Short term fuel trim (bank 1)',
+          value: decodedata.calculate_fuel_Trim(hexdata)
+        };
       case '08':
-        break;
+        return {
+          id: '08',
+          name: 'Short term fuel trim (bank 1)',
+          value: decodedata.calculate_fuel_Trim(hexdata)
+        };
       case '09':
-        break;
+        return {
+          id: '09',
+          name: 'Short term fuel trim (bank 1)',
+          value: decodedata.calculate_fuel_Trim(hexdata)
+        };
       case '0A':
         return {
           id: '0A',
@@ -99,7 +127,11 @@ export function decodeFrame(data) {
           value: decodedata.cal_throttle_position(hexdata)
         };
       case '12':
-        break;
+        return {
+          id: '12',
+          name: 'Commanded secondary air status',
+          value: decodedata.comanded_secondary_air_status(hexdata)
+        };
 
       case '13':
         return {
@@ -167,6 +199,76 @@ export function decodeFrame(data) {
           name: 'Run time since engine start',
           value: decodedata.cal_run_time_engine_start(hexdata)
         };
+
+
+      //==============================================================================================================================///   PID 61 -7F OR 80
+
+      case '61':
+        return {
+          id: '61',
+          name: 'Drivers demand engine - percent torque',
+          value: decodedata.drivers_demand_engine_precent_torque(hexdata)
+        };
+
+      case '62':
+        return {
+          id: '62',
+          name: 'Actual engine - percent torque',
+          value: decodedata.actual_engine_percent_torque(hexdata)
+        };
+      case '63':
+        return {
+          id: '63',
+          name: 'Engine reference torque',
+          value: decodedata.engine_refernce_torque(hexdata)
+        };
+
+      case '64':
+        return {
+          id: '64',
+          name: 'Engine percent torque data',
+          value: decodedata.engine_percent_torque_data(hexdata)
+        };
+
+      case '65':
+        return {
+          id: '65',
+          name: 'Auxiliary input / output supported',
+          value: decodedata.auxillory_input_output_support(hexdata)
+        };
+
+
+      case '66':
+        return {
+          id: '66',
+          name: 'Mass air flow sensor',
+          value: decodedata.mass_air_flow_sensor(hexdata)
+        };
+
+      case '67':
+        return {
+          id: '67',
+          name: 'Engine coolant temperature 1',
+          value: decodedata.engine_colent_sensor_1(hexdata)
+        };
+
+      case '68':
+        return {
+          id: '68',
+          name: 'Intake air temperature sensor 1',
+          value: decodedata.intake_air_temperture_sensor_1(hexdata)
+        };
+
+      
+      case '7C':
+        return {
+          id: '7C',
+          name: 'Diesel Particulate filter (DPF) temperature',
+          value: decodedata.diesel_particulate_filter_temperture(hexdata)
+        };
+
+
+
       default:
         console.error(`Unknown PID: ${hexdata.slice(0, 2)}`);
         return null;
